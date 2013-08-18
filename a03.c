41ce89ecd3650d0b4ac42c16e9512b388207a461
Randall Sewell <randallsewell@gmail.com>
2013-08-17 23:26:47 -0700

    Changed A03 to conform to spec; Changed A04 to proper implementation of loop optimization.

--- a03.c
+++ a03.c
@@ -11,14 +11,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
     int 	fd[2];
     pid_t 	childpid;
-    char 	readbuffer[80] = { 0 };
     
     // set up pipe
     pipe(fd);
@@ -27,22 +26,32 @@ int main(int argc, char **argv)
     childpid = fork();
 
     if ( childpid == 0 ) {
-        // -- running in child process --
+            // -- running in child process --
 
 
-        // Receive characters from parent process via pipe
-        // one at a time, and count them.
-        // Return number of characters counted to parent process.
-        int     nChars = 0;
-        close(fd[1]);
-        nChars = read(fd[0], readbuffer, sizeof(readbuffer));
+            // Receive characters from parent process via pipe
+            // one at a time, and count them.
+            // Return number of characters counted to parent process.
+        int     nChars = 0; // Counter for characters
+        char    buffer;     // Character buffer
+        
+        // Close unused end of pipe
+            close(fd[1]);
 
+        // Read characters one at a time from pipe
+        while ( read(fd[0], &buffer, 1) > 0 )
+            nChars++; // Increment count per character read
+
+        // Close current end of pipe
+        close(fd[0]);
         exit(nChars);
     } else {
         // -- running in parent process --
         
-        int 	nChars = 0;
-        int 	status;
+        int 	nChars = 0; // To store exit code form child process
+        int 	status;     // Honestly, I'm not 100% clear on how this works
+        int     i;          // Loop variable for argument number
+        int     j;          // Loop varuabel for character number
         printf("CS201 - Assignment 3 - Randall Sewell\n");
         
         // Send characters from command line arguments starting with
@@ -50,16 +59,30 @@ int main(int argc, char **argv)
         // Wait for child process to return. Reap child process.
         // Receive number of characters counted via the value
         // returned when the child process is reaped.
+
+        // Close unused end of pipe
         close(fd[0]);
-        write(fd[1], argv[1], (strlen(argv[1])));
         
+        // Write characters to pipe one at a time
+        for (i = 1; i < argc; i++ ) {
+            for (j = 0; j < strlen(argv[i]); j++) {
+                write(fd[1], &argv[i][j], 1);
+            }
+        }
+
+        // Close used end of pipe
+        close(fd[1]);
+
+        // Wait for child to terminate and reap with exit status equal to number of characters counted
         while ((childpid = waitpid(-1, &status, 0)) > 0 ) {
             if ( WIFEXITED(status) )
                 nChars = WEXITSTATUS(status);
             else
                 printf("Abnormal termination of child %d", childpid);
         }
-        
+        // Output characters counted
+        // Interesting, but not surprising, behavior: some characters aren't counted unless escaped
+        // e.g. "./a03" ### returns 0, but "./a03 \#" returns 1
         printf("child counted %d chars\n", nChars);
         return 0;
     }
--- a04.c
+++ a04.c
@@ -12,35 +12,34 @@ int main (void)
     double  *array = calloc(ARRAY_SIZE, sizeof(double));
     double  sum = 0;
     int     i;
-  
 
     // You can add variables between this comment ...
-    double  arraySum = 0;
-    int     arrayFlag = 0;
+    double *ptr;
+    double sum1 = 0;
+    double sum2 = 0;
     // ... and this one.
 
     // Please change 'your name' to your actual name.
     printf("CS201 - Asgmt 4 - Randall Sewell\n");
     for (i = 0; i < N_TIMES; i++) {
+
     // You can change anything between this comment ...
-        int     j;
-        if ( arrayFlag == 0 ) {
-            double accum = 0;
-            for (j = 0; j < ARRAY_SIZE; j++) {
-                accum += array[j];
-            }
-            arraySum = accum;
-            arrayFlag = 1;
+        sum1 = 0;
+        sum2 = 0;
+        for (ptr = array; ptr < &array[ARRAY_SIZE]; ptr += 12) {
+            sum1 += (*ptr) + *(ptr + 1) + *(ptr + 2) + *(ptr + 3) + *(ptr + 4) + *(ptr + 5);
+            sum2 += *(ptr + 6) + *(ptr + 7) + *(ptr + 8) + *(ptr + 9) + *(ptr + 10) + *(ptr + 11);
         }
-        sum += arraySum;
+        sum += sum1 + sum2;		
     // ... and this one. But your inner loop must do the same
     // number of additions as this one does.
+
     }
+
     // You can add some final code between this comment ...
-    printf("You cannot make a thing idiot-proof;\n");
-    printf("you can only make it idiot-resistant.\n");
-    printf("So long, and thanks for all the fish.\n");
-    printf("\nThanks for a great class!\n");
+    // Final time-check is 6.247s averaged across 5 consecutive runs
+    // It's a little higher now, actually, so just ignore that
     // ... and this one.
+
     return 0;
 }
